8831aaeb58991aa9dd81081ea5db4c149ff32909
William Ekengren <h23wieke@du.se>
2023-12-22 13:45:06 +0100

    TESPUSH PLS

---
+++ Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATIONDAYBOOK
+{
+    public class Post
+    {
+        //Sparar inläggen
+        public int dateTime { get; set; }
+        public string postTitle { get; set; }
+        public string post { get; set; }
+
+
+    }
+}
---
+++ PostHanterare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATIONDAYBOOK
+{
+    public class PostHanterare
+    {
+        static List<Post> postList = new List<Post>();
+        static string postSave = "posts.txt"; // Filnamnet där posten sparas.
+    }
+    static void PostSave()
+    {
+        //StreamWriter
+      /*  savePost(); Denna kommer att ladda filen när programmet startas, om det redan finns sparade inlägg */
+      using (StreamWriter writer = new StreamWriter(postSave))
+        {
+            foreach (var post in postLista)
+            {
+                writer.WriteLine($"{post.Titel}|{post.postTitle}");
+            }
+        }
+    }
+}
